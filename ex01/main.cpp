b797b182c3cef7e73a2941db04a5c784585e11cf
Louis B <lb@gmail.com>
2024-09-15 22:45:28 +0200

    improved main readability

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,13 @@
 int main( void ) {
     int i;
     int arr[5] = {0, 1, 2, 3, 4};
+
     std::cout << "Array is:";
     for (i = 0;i<5;i++)
         std::cout << " " << arr[i]; 
+
     std::cout << std::endl << "Checking each element using the iter function:";
     iter(arr, 5, printElement<int>);
+    
     return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,12 +4,14 @@ int main( void ) {
     int i;
     std::string testStr = "Hola";
     float testFloat = 42.42;
+
     Array<std::string> testArrayStr(5);
     Array<float> testArrayFloat(5);
 
     std::cout << "String=" << testStr << std::endl; 
     for (i = 0;i<5;i++)
         testArrayStr[i] = testStr;
+
     std::cout << "Content of the array:" << std::endl;
     for (i = 0;i<5;i++)
         std::cout << "Array[" << i << "]=" << testArrayStr[i] << std::endl;
@@ -17,9 +19,10 @@ int main( void ) {
     std::cout << std::endl << "Float=" << testFloat << std::endl; 
     for (i = 0;i<5;i++)
         testArrayFloat[i] = testFloat;
+
     std::cout << "Content of the array:" << std::endl;
     for (i = 0;i<5;i++)
         std::cout << "Array[" << i << "]=" << testArrayFloat[i] << std::endl;
-    
+        
     return 0;
 }
